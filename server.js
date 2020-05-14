b9ff14542058c7e6cb5c4828b35b5c819b7bd94d
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-14 22:14:52 +0200

    server

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(22000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:22000')
   })
 })
 .catch((ex) => {
@@ -30,7 +30,7 @@ app.prepare()
 })
 
 
-// const port = parseInt(process.env.PORT, 10) || 3000
+// const port = parseInt(process.env.PORT, 10) || 22000
 // const dev = process.env.NODE_ENV !== 'production'
 // const app = next({ dev })
 // const handle = app.getRequestHandler()
