e2e884ac46a9fa290fcb893ebcba3db3da2d3369
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-15 20:17:20 +0200

    server

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.prepare()
   const server = express()
   server.use((req, res) => handle(req, res));
 
-  server.get('*', (req, res) => {
+  server.get('/', (req, res) => {
     return handle(req, res)
   })
 
-  server.post('/api/sendmail', (req, res) => {
-    return handle(req, res)
-  })
+  // server.post('/api/sendmail', (req, res) => {
+  //   return handle(req, res)
+  // })
 
   server.listen(41160, '0.0.0.0', (err) => {
     if (err) throw err
@@ -28,27 +28,3 @@ app.prepare()
   console.error(ex.stack)
   process.exit(1)
 })
-
-
-// const port = parseInt(process.env.PORT, 10) || 41160
-// const dev = process.env.NODE_ENV !== 'production'
-// const app = next({ dev })
-// const handle = app.getRequestHandler()
-
-// app.prepare().then(() => {
-//   const server = express()
-
-//   // server.get('/', (req, res) => {
-//   //   return app.render(req, res, '/', req.query)
-//   // })
-
-
-//   server.all('*', (req, res) => {
-//     return handle(req, res)
-//   })
-
-//   server.listen(port, err => {
-//     if (err) throw err
-//     console.log(`> Ready on http://localhost:${port}`)
-//   })
-// })
