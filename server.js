e128027067be46d3955a5ac1e72dc4a780cbd2a2
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-15 18:00:35 +0200

    por

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, '::', (err) => {
+  server.listen(3000, '0.0.0.0', (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
