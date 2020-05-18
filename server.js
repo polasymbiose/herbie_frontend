57614c9761b2925ff7d3e9d569ae69eb3ed0ac2a
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-18 23:05:25 +0200

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 41160
 app.prepare()
 .then(() => {
   const server = express()
