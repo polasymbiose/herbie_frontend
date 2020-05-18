0418e8c6b9b0883d28fedf20fe69a38e4d14e406
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-18 23:27:52 +0200

    prod

---
+++ prod.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const React = require('react')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const port = process.env.PORT || 3022
+app.prepare()
+.then(() => {
+  const server = express()
+  server.use((req, res) => handle(req, res));
+
+  server.get('/', (req, res) => {
+    return handle(req, res)
+  })
+
+  // server.post('/api/sendmail', (req, res) => {
+  //   return handle(req, res)
+  // })
+
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log('> Ready on 0.0.0.0:' + port)
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
