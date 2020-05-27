fe0c37019ace8c68de59d890981b3736877001ef
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-27 17:34:04 +0200

    update start script

--- prod.js
+++ prod.js
@@ -5,7 +5,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-const port = process.env.PORT || 3022
+const port = 3022
 app.prepare()
 .then(() => {
   const server = express()
