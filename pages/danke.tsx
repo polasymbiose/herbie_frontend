18c3bc2a4848d8df8c4f4e26915d1b036f16b416
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2020-05-27 19:00:18 +0200

    danke seite

---
+++ pages/danke.tsx
@@ -0,0 +1,23 @@
+// import Link from 'next/link';
+import React from 'react'
+import { fetchWrapper, host } from '../src/helper/fetch'
+import App from '../src/App'
+import getUnsplashImage from '../lib/getUnsplashImage'
+
+function Index(props) {
+  return <App alldata={props.sites} priceCat={props.mainCategories} {...props} />
+}
+
+export async function getStaticProps() {
+  const url = `${host()}`
+  const res = await fetchWrapper(`${url}/sites/`)
+  const sites = await res.json()
+  
+  return {
+    props: {
+      sites
+    }
+  }
+}
+
+export default Index
